5fd8eb511fa9d3ceccceae8c97e971a6626da9a7
mariliagaldinor <mariliagaldinor@gmail.com>
2022-09-24 17:28:34 -0400

    criando um arquivo html

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function calcularIdade(){
+let nome = document.getElementById('nome').value
+let nascimento = document.getElementById('idade').value
+let idade = 2022 - nascimento
+alert(idade)
+}
